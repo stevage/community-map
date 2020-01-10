5c84be22e9c28cc19170d870601d947cbc237855
Steve Bennett <stevage@gmail.com>
2020-01-10 13:44:16 +1100

    Support image_url etc.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,27 +5,7 @@
 <script>
 import mapboxgl from 'mapbox-gl';
 import U from 'mapbox-gl-utils';
-const d3 = require('d3-fetch');
-
-// Replace this URL with your own Google Sheets link
-const csvSource = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR9qYMju-qqH_IL8e2ksN0wpVXfHBUEKF079WX1eSAgPFRG5z0RAmpjVwS8sVrZSC0fVrNpSMjaB5Cu/pub?gid=0&single=true&output=csv';
-
-function toPoints(rows) {
-    return {
-        type: 'FeatureCollection',
-        features: rows.map((row, id) => ({
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [+row.Longitude, +row.Latitude], // These are the column names
-            },
-            properties: {
-                id,
-                ...row
-            }
-        }))
-    }
-}
+import { sheets2geojson } from 'sheets2geojson';
 
 export default {
     async mounted() {
@@ -37,7 +17,7 @@ export default {
             zoom: 14,
             style: 'mapbox://styles/mapbox/light-v9',
         });
-        U.init(map);
+        U.init(map, mapboxgl);
         window.map = map;
         window.app.Map = this;
 
@@ -50,7 +30,6 @@ export default {
         const sheetID = (window.location.hash.match(/sheet=([a-zA-Z0-9_-]+)/) || [])[1] || '2PACX-1vR9qYMju-qqH_IL8e2ksN0wpVXfHBUEKF079WX1eSAgPFRG5z0RAmpjVwS8sVrZSC0fVrNpSMjaB5Cu';
         const points = await sheets2geojson(sheetID);
 
-        const points = toPoints(await d3.csv(csvSource));
         map.U.addGeoJSON('points', points);
         map.U.addCircle('points-circles', 'points', {
             circleColor: 'hsl(330,100%,40%)',
