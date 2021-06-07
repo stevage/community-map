159304dc405d485b6f8991612b92f234d427d033
Steve Bennett <steveben@fb.com>
2021-06-07 19:18:51 +1000

    Don't need src/static

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     publicPath: '',
     devServer: {
@@ -6,19 +5,6 @@ module.exports = {
         port: 8080,
         https: false,
         // open: true, // automatically open browser
-    }, 
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin([
-                {
-                    // because the images aren't statically referred to anywhere, they don't bundled by webpack
-                    // so we have to explicitly copy them to where we want them
-                    from: 'src/static',
-                    to: 'static',
-                    transformPath: (targetPath, absolutePath) => targetPath.toLowerCase() 
-                }
-            ])
-        ]
     },
-    chainWebpack: config => config.resolve.symlinks(false)
+    chainWebpack: (config) => config.resolve.symlinks(false),
 };
